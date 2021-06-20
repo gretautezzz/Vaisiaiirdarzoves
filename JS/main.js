093adbc47a0148799dff520779c735470d57eee4
Tumas11 <tumaradz@inbox.lt>
2021-06-20 12:56:30 +0300

    JavaScriptas filter ir search funkcijoms

--- JS/main.js
+++ JS/main.js
@@ -1,17 +1,46 @@
 var cards = document.getElementsByClassName("card");
 var btn = document.getElementById ("btn")
-console.log(cards[0].getAttribute("id"));
-console.log(cards[1].getAttribute("id"));
 
 var manoforma = document.forms["input"];
 
 manoforma.addEventListener("submit", function(e) {
     e.preventDefault();
-var input = manoforma["input2"].value;
+var input = manoforma["input2"].value.toLowerCase();
 console.log(input);
     for (var i=0; i < cards.length; i++ ){
         var id = cards[i].getAttribute("id");
-        console.log(cards[i].getAttribute("id"));
+        cards[i].classList.remove("hide"); 
+        if (input == id){
+            cards[i].classList.add("show");   
+        } else{
+            cards[i].classList.add("hide"); 
+        }
     }
 })
 
+var fruits = document.getElementsByClassName("fruits");
+var vegetables = document.getElementsByClassName("vegetables");
+
+var select = document.getElementById("select");
+select.addEventListener("change", function() {
+   if (select.value == "fruits"){
+   for (var i=0; i < fruits.length; i++ ){
+    fruits[i].classList.remove("hide"); 
+    vegetables[i].classList.remove("hide"); 
+       fruits[i].classList.add("show");
+   }
+   for (var i=0; i < vegetables.length; i++){
+       vegetables[i].classList.add("hide"); 
+   } } else {
+    for (var i=0; i < fruits.length; i++ ){
+        fruits[i].classList.remove("hide"); 
+        vegetables[i].classList.remove("hide"); 
+    
+        fruits[i].classList.add("hide");
+    }
+    for (var i=0; i < vegetables.length; i++){
+        vegetables[i].classList.add("show"); 
+   }
+   }
+
+})
